5debf81b08916542d96337dd161912bd6be95621
Priyansh Patel <111214802+Priyansh-Patel-India@users.noreply.github.com>
2023-10-19 08:31:44 +0530

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 8080;
-export const DB_URL =
-	"mongodb+srv://priyansh:HgJGz3NmihQH7ecc@cluster.vauciy5.mongodb.net/test?retryWrites=true&w=majority";
-export const SECRET_KEY = "HgJGz3NmihQH7ecc";
